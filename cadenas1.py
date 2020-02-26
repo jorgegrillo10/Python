9d05db81da996a765bbd753e552f049adb23fe21
jorgegrillo10 <44004795+jorgegrillo10@users.noreply.github.com>
2020-02-26 12:07:14 +0100

    Create cadenas1.py

---
+++ cadenas1.py
@@ -0,0 +1,16 @@
+#Comillas Simples
+cads='Texteo entre \n comillas simples'
+#Comillas doble
+cadd="Texto entre \n\t comillas dobles"
+
+cadc= """Texto linea 1
+linea2
+line3
+line4
+.
+.
+.
+lineN
+"""
+
+print cadc
