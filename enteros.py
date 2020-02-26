ac2759e437d651b89eb5bc4f622e3db7aa710fb3
jorgegrillo10 <44004795+jorgegrillo10@users.noreply.github.com>
2020-02-26 12:05:59 +0100

    Create enteros.py

---
+++ enteros.py
@@ -0,0 +1,25 @@
+a=26
+b=11.3
+c=5
+d=3.5
+
+#SUMA
+print a+b
+
+#REST
+print c-a
+
+#multip
+print d*c
+
+#exponente
+print c**2
+
+#Division con decimales
+print float(c)/a 
+
+#Division entera
+print float(7)/3
+
+#Modulo
+print 7%3
